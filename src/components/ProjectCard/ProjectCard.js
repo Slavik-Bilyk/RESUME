91ead022361a3fdd2792dcf9e532a1cf31bdbdf2
Slavik-Bilyk <Slavataschor@ukr.net>
2025-01-06 15:53:23 +0200

    add video in project

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,23 +2,27 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import { FaArrowRight } from "react-icons/fa";
 
-const ProjectCard = ({ title, description, imgUrl, link }) => {
-  const linkRef = React.useRef(null);
-
-  return (
-    <Col sm={6} md={4}>
-      <div className='project-img'>
-        <img src={imgUrl} alt={title} />
-        <div className="project-text">
-          <h4>{title}</h4>
-          <span>{description}</span>
-          <a href={link} target='_blank' rel='noopener noreferrer' ref={linkRef}>
-            <FaArrowRight size={20}/>
-          </a>
-        </div>
-      </div>
-    </Col>
-  );
-}
+const ProjectCard = ({ title, description, videoUrl, link }) => {
+    return (
+        <Col sm={6} md={4}>
+            <div className='project-img'>
+                <video 
+                    src={videoUrl} 
+                    autoPlay 
+                    loop 
+                    muted 
+                    className="project-video"
+                />
+                <div className="project-text">
+                    <h4>{title}</h4>
+                    <span>{description}</span>
+                    <a href={link} target='_blank' rel='noopener noreferrer'>
+                        <FaArrowRight size={20} />
+                    </a>
+                </div>
+            </div>
+        </Col>
+    );
+};
 
 export default ProjectCard;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,105 +1,93 @@
-import React from 'react'
-import projImg1 from '../../images/project-img1.png'
-import projImg2 from '../../images/project-img2.png'
-import projImg3 from '../../images/project-img3.png'
-import projImg4 from '../../images/project-img4.png'
-import projImg5 from '../../images/project-img5.png'
-
-import colorSharp2 from '../../images/color-sharp2.png'
-import { Col, Container, Tab, Nav, NavLink, Row, TabContainer, TabContent } from 'react-bootstrap'
-import ProjectCard from '../ProjectCard/ProjectCard'
-import { GiClick } from "react-icons/gi";
-
+import React from 'react';
+import video1 from '../../assets/videos/Video1.mp4';
+import video2 from '../../assets/videos/Video2.mp4';
+import colorSharp2 from '../../images/color-sharp2.png';
+import { Col, Container, Tab, Nav, NavLink, Row, TabContainer, TabContent } from 'react-bootstrap';
+import ProjectCard from '../ProjectCard/ProjectCard';
+import { GiClick } from 'react-icons/gi';
 import './Projects.css';
 
-
-
 const Projects = () => {
+    const projects = [
+        {
+            title: 'WebStudio',
+            description: 'HTML/CSS',
+            videoUrl: video1,
+            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+        },
+        {
+            title: 'House Staff',
+            description: 'React/styled-components',
+            videoUrl: video2, 
+            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+        },
+        {
+            title: 'MoviesAPI',
+            description: 'Router/API',
+            videoUrl: video1, 
+            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+        },
+        {
+            title: 'WebStudio',
+            description: 'HTML/CSS',
+            videoUrl: video1,
+            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+        },
+        {
+            title: 'House Staff',
+            description: 'React/styled-components',
+            videoUrl: video1, 
+            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+        },
+        {
+            title: 'MoviesAPI',
+            description: 'Router/API',
+            videoUrl: video1, 
+            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+        },
+    ];
 
-const projects = [
-    {
-        title: 'WebStudio',
-        description: 'HTML/CSS',
-        imgUrl: projImg1,
-        link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
-    },
-    {
-        title: 'House Staff',
-        description: 'React/styled-components',
-        imgUrl: projImg2,
-        link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
-    },
-    {
-        title: 'MoviesAPI',
-        description: 'Router/API',
-        imgUrl: projImg3,
-        link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
-    },
-    {
-        title: 'My first portfolio',
-        description: 'React',
-        imgUrl: projImg4,
-        link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
-    },
-    {
-        title: 'Game Store',
-        description: 'React, CSS',
-        imgUrl: projImg5,
-        link: 'https://slavik-bilyk.github.io/gamestore/',
-    },
-    {
-        title: 'Business startup',
-        description: 'Design development',
-        imgUrl: projImg3,
-        link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
-    }
-]
+    return (
+        <section className="projects" id="projects">
+            <Container>
+                <h2>Projects</h2>
+                <p>This section presents all my significant works from the entire time I've been programming</p>
+                <TabContainer id="projects-tabs" defaultActiveKey="first">
+                    <Nav className="nav-pills">
+                        <Nav.Item>
+                            <NavLink eventKey="first">Projects</NavLink>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <NavLink eventKey="two">Github</NavLink>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <NavLink eventKey="three">...</NavLink>
+                        </Nav.Item>
+                    </Nav>
+                    <TabContent className="card">
+                        <Tab.Pane eventKey="first">
+                            <Row>
+                                {projects.map((project, index) => (
+                                    <ProjectCard key={index} {...project} />
+                                ))}
+                            </Row>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="two">
+                            <p>
+                                <a href="https://github.com/Slavik-Bilyk" target="_blank" rel="noopener noreferrer">
+                                    Go to my GitHub <GiClick />
+                                </a>
+                            </p>
+                        </Tab.Pane>
+                        <Tab.Pane eventKey="three">
+                            <p>If you liked this site, leave a request below so we can talk</p>
+                        </Tab.Pane>
+                    </TabContent>
+                </TabContainer>
+            </Container>
+            <img src={colorSharp2} className="bg-image" alt="Background" />
+        </section>
+    );
+};
 
-return (
-    <section className='projects' id='projects'>
-     
-     <Container>
-          
-             <h2>Projects</h2>
-             <p>This section presents all my significant works from the entire time I've been programming</p>
-             <TabContainer id="projects-tabs" defaultActiveKey="first">
-                 <Nav className='nav-pills'>
-                     <Nav.Item>
-                         <NavLink eventKey="first">Projects</NavLink>
-                     </Nav.Item>
-                     <Nav.Item>
-                         <NavLink eventKey="two">Github</NavLink>
-                     </Nav.Item>
-                     <Nav.Item>
-                         <NavLink eventKey="three">...</NavLink>
-                     </Nav.Item>
-                 </Nav>
-                 <TabContent className='card' >
-                     <Tab.Pane eventKey='first'>
-                         <Row>
-                             {
-                                 projects.map((project, index) => {
-                                     return (
-                                        <ProjectCard key={index} {...project}/>
-                                     )
-                                 })
-                             }
-                         </Row>
-                     </Tab.Pane>
-                     <Tab.Pane eventKey="two">
-                      <p><a href='https://github.com/Slavik-Bilyk' target='_blank'>Go to mine GitHub<GiClick/></a></p>
-                     </Tab.Pane>
-                     <Tab.Pane eventKey="three">
-                       <p>If you liked this site, leave a request below so we can talk</p>
-                     </Tab.Pane>
-                 </TabContent>
-             </TabContainer>
-
-     </Container>
-     <img src={colorSharp2} className='bg-image'/>
-    </section>
- )
- 
-}
-
-export default Projects
+export default Projects;
