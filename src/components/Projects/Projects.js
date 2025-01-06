05912bc62b7395e211bdc637050f5b7646567cd3
Slavik-Bilyk <Slavataschor@ukr.net>
2025-01-06 16:52:17 +0200

    add new video

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import video1 from '../../assets/videos/Video1.mp4';
 import video2 from '../../assets/videos/Video2.mp4';
+import video3 from '../../assets/videos/Video3.mp4';
+import video4 from '../../assets/videos/Video4.mp4';
+import video5 from '../../assets/videos/Video5.mp4';
 import colorSharp2 from '../../images/color-sharp2.png';
 import { Col, Container, Tab, Nav, NavLink, Row, TabContainer, TabContent } from 'react-bootstrap';
 import ProjectCard from '../ProjectCard/ProjectCard';
@@ -13,45 +16,40 @@ const Projects = () => {
             title: 'WebStudio',
             description: 'HTML/CSS',
             videoUrl: video1,
-            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+            link: 'https://github.com/Slavik-Bilyk/goit-markup-hw-08',
         },
         {
-            title: 'House Staff',
-            description: 'React/styled-components',
+            title: 'Game Store',
+            description: 'React/CSS',
             videoUrl: video2, 
-            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+            link: 'https://github.com/Slavik-Bilyk/gamestore',
         },
         {
             title: 'MoviesAPI',
             description: 'Router/API',
-            videoUrl: video1, 
-            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+            videoUrl: video3, 
+            link: 'https://github.com/Slavik-Bilyk/MoviesAPI',
         },
         {
-            title: 'WebStudio',
+            title: 'My first Portfolio',
             description: 'HTML/CSS',
-            videoUrl: video1,
-            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+            videoUrl: video4,
+            link: 'https://github.com/Slavik-Bilyk/Portfolio',
         },
         {
-            title: 'House Staff',
-            description: 'React/styled-components',
-            videoUrl: video1, 
-            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
+            title: 'Events',
+            description: 'React/Node.js',
+            videoUrl: video5, 
+            link: 'https://github.com/Slavik-Bilyk/EventsRegistrationApp',
         },
-        {
-            title: 'MoviesAPI',
-            description: 'Router/API',
-            videoUrl: video1, 
-            link: 'https://slavik-bilyk.github.io/goit-markup-hw-08/',
-        },
+       
     ];
 
     return (
         <section className="projects" id="projects">
             <Container>
                 <h2>Projects</h2>
-                <p>This section presents all my significant works from the entire time I've been programming</p>
+                <p>TThis section presents some of my projects that I wrote during my studies. Of course, this is not all, you can get acquainted with others on my Github</p>
                 <TabContainer id="projects-tabs" defaultActiveKey="first">
                     <Nav className="nav-pills">
                         <Nav.Item>
